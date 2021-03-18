6dcf3bf5712f1f5fc17731fc8113313973825b4a
Patroi Alex <67483871+Alex99P@users.noreply.github.com>
2021-03-18 23:41:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,26 +7,56 @@ const transport=document.getElementById("valTransport");
 
 
 
+function calculator1(e)
+{ 
+    if(e.key=='Enter'){
+    
+      if(pret.value=="" || factura.value=="" || transport.value==""){
+        alert("Trebuie completate toate casutele!")
+        return
+      }
+    
+      // var pretTotal=pret.value*moneda.value*1.19+(pret.value*(parseInt(factura.value)+parseInt(transport.value))/factura.value);
+
+    
+      var pretTotal=pret.value*(parseFloat(factura.value)+(parseFloat(transport.value)/moneda.value))/(parseFloat(factura.value))*1.19*moneda.value;
+
+      // console.log(pretTotal.toFixed(2));
+      document.getElementById("pretAcasa").value=pretTotal.toFixed(2);
+      document.getElementById("pret100").value=(pretTotal*2).toFixed(2);
+      document.getElementById("pret150").value=(pretTotal*2.5).toFixed(2);
+      document.getElementById("pret200").value=(pretTotal*3).toFixed(2);
+    }   
+
+}
+function calculator(e)
+{ 
+    
+      if(pret.value=="" || factura.value=="" || transport.value==""){
+        alert("Trebuie completate toate casutele!")
+        return
+      }
+    
+      // var pretTotal=pret.value*moneda.value*1.19+(pret.value*(parseInt(factura.value)+parseInt(transport.value))/factura.value);
+
+    
+      var pretTotal=pret.value*(parseFloat(factura.value)+(parseFloat(transport.value)/moneda.value))/(parseFloat(factura.value))*1.19*moneda.value;
+
+      // console.log(pretTotal.toFixed(2));
+      document.getElementById("pretAcasa").value=pretTotal.toFixed(2);
+      document.getElementById("pret100").value=(pretTotal*2).toFixed(2);
+      document.getElementById("pret150").value=(pretTotal*2.5).toFixed(2);
+      document.getElementById("pret200").value=(pretTotal*3).toFixed(2);
+    
+
+}
+
+
+
 var buton=document.getElementById("btn");
-buton.addEventListener("click",function(){
+buton.addEventListener('click',calculator);
 
-  if(pret.value=="" || factura.value=="" || transport.value==""){
-    alert("Trebuie completate taote casutele!")
-    return
-  }
-
-  // var pretTotal=pret.value*moneda.value*1.19+(pret.value*(parseInt(factura.value)+parseInt(transport.value))/factura.value);
-
-  var pretTotal=pret.value*(parseFloat(factura.value)+(parseFloat(transport.value)/moneda.value))/(parseFloat(factura.value))*1.19*moneda.value;
-
-  // console.log(pretTotal.toFixed(2));
-  document.getElementById("pretAcasa").value=pretTotal.toFixed(2);
-  document.getElementById("pret100").value=(pretTotal*2).toFixed(2);
-  document.getElementById("pret150").value=(pretTotal*2.5).toFixed(2);
-  document.getElementById("pret200").value=(pretTotal*3).toFixed(2);
-
-
-
-});
-
+document.getElementById("valTransport").addEventListener("keypress",calculator1);
+document.getElementById("pret").addEventListener("keypress",calculator1);
+document.getElementById("valFactura").addEventListener("keypress",calculator1);
 
